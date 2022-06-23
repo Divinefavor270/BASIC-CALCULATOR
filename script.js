d4271859eb336ba60e2e01a0c5c51e3d7f1215fb
Cynthia <105214676+Divinefavor270@users.noreply.github.com>
2022-06-22 20:28:40 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// addition of numbers
+var num1 = prompt("Enter a number");
+var num2 =prompt("Enter another number");
+
+// convert both strings to actual numbers 
+ num1 = parseFloat(num1);
+ num2 = parseFloat(num2);
+
+document.write(num1+num2)
